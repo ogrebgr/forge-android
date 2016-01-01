a1a0ce762e1471cbb6c15a737f42d5f75a9f24c7
Ognyan Bankov <ogibankov@gmail.com>
2016-01-01 09:35:21 +0200

    moving Android dependent tests to androidTest

--- forge-android/src/androidTest/java/com/bolyartech/forge/app_unit/Act1.java
+++ forge-android/src/androidTest/java/com/bolyartech/forge/app_unit/Act1.java
@@ -1,9 +1,12 @@
 package com.bolyartech.forge.app_unit;
 
+import android.app.Activity;
+
+
 /**
  * Created by ogre on 2015-10-22
  */
-public class Act1 extends ActivityComponentImpl {
+public class Act1 extends Activity implements ActivityComponent {
     @Override
     public ResidentComponent createResidentComponent() {
         return null;
--- forge-android/src/androidTest/java/com/bolyartech/forge/app_unit/Act2.java
+++ forge-android/src/androidTest/java/com/bolyartech/forge/app_unit/Act2.java
@@ -3,7 +3,7 @@ package com.bolyartech.forge.app_unit;
 /**
  * Created by ogre on 2015-10-22
  */
-public class Act2 extends ActivityComponentImpl {
+public class Act2 extends Activity implements ActivityComponent {
     @Override
     public ResidentComponent createResidentComponent() {
         return null;
