05cdda10120f05217777a3d2f3cf681659438268
Ognyan Bankov <ogibankov@gmail.com>
2016-01-06 09:16:46 +0200

    nullify ref to activity on stop

--- forge-android/src/main/java/com/bolyartech/forge/app_unit/AbstractResidentComponent.java
+++ forge-android/src/main/java/com/bolyartech/forge/app_unit/AbstractResidentComponent.java
@@ -38,6 +38,7 @@ abstract public class AbstractResidentComponent implements ResidentComponent {
         if (mActivity.isFinishing()) {
             die();
         }
+        mActivity = null;
     }
 
 
