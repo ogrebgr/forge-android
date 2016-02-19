17252479732455c6a5fbb04749de4f4b1ce94902
Ognyan Bankov <obankov@objectsystems.com>
2016-02-19 16:51:22 +0200

    adding (silent) reset to StateManager

--- forge-android/src/main/java/com/bolyartech/forge/android/app_unit/StateManager.java
+++ forge-android/src/main/java/com/bolyartech/forge/android/app_unit/StateManager.java
@@ -3,4 +3,5 @@ package com.bolyartech.forge.android.app_unit;
 public interface StateManager<T extends Enum<T>> {
     T getState();
     void switchToState(T state);
+    void reset();
 }
--- forge-android/src/main/java/com/bolyartech/forge/android/app_unit/StateManagerImpl.java
+++ forge-android/src/main/java/com/bolyartech/forge/android/app_unit/StateManagerImpl.java
@@ -7,12 +7,14 @@ import com.bolyartech.forge.android.misc.AndroidEventPoster;
 public class StateManagerImpl<T extends Enum<T>> implements StateManager<T> {
     private static final StateChangedEvent mStateChangedEvent = new StateChangedEvent();
     private T mState;
+    private T mDefaultState;
     private final AndroidEventPoster mPoster;
 
 
     public StateManagerImpl(AndroidEventPoster poster, T initialState) {
         mPoster = poster;
         mState = initialState;
+        mDefaultState = initialState;
     }
 
 
@@ -24,5 +26,11 @@ public class StateManagerImpl<T extends Enum<T>> implements StateManager<T> {
     public void switchToState(T state) {
         mState = state;
         mPoster.postEvent(mStateChangedEvent);
+    }
+
+
+    @Override
+    public void reset() {
+        mState = mDefaultState;
     }
 }
