a74ddc675678e9102977b98fc0c6c583a43586e7
Ognyan Bankov <ogibankov@gmail.com>
2016-07-16 17:16:46 +0300

    adding utility clas

---
+++ forge-android/src/main/java/com/bolyartech/forge/android/misc/ExternalStorageHelper.java
@@ -0,0 +1,40 @@
+package com.bolyartech.forge.android.misc;
+
+import android.os.Environment;
+
+
+public class ExternalStorageHelper {
+    private ExternalStorageHelper() {
+        throw new AssertionError("Non-instantiable utility class");
+    }
+
+
+    public static ExternalStorageState checkState() {
+        boolean externalStorageAvailable = false;
+        boolean externalStorageWritable = false;
+
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            externalStorageAvailable = externalStorageWritable = true;
+        } else if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            externalStorageAvailable = true;
+            externalStorageWritable = false;
+        } else {
+            externalStorageAvailable = externalStorageWritable = false;
+        }
+
+        return new ExternalStorageState(externalStorageAvailable, externalStorageWritable);
+    }
+
+
+    public static class ExternalStorageState {
+        public final boolean isAvailable;
+        public final boolean isWritable;
+
+
+        public ExternalStorageState(boolean isAvailable, boolean isWritable) {
+            this.isAvailable = isAvailable;
+            this.isWritable = isWritable;
+        }
+    }
+}
