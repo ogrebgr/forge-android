7220458e5903adea5236547414f1da832884c9ff
Ognyan Bankov <ogibankov@gmail.com>
2016-08-31 17:54:22 +0300

    removing unused file

--- forge-android/src/main/java/com/bolyartech/forge/android/app_unit/AppEvent.java
+++
@@ -1,7 +0,0 @@
-package com.bolyartech.forge.android.app_unit;
-
-/**
- * Marker interface intended to be used as base for application events that will be broadcast by the bus (otto)
- */
-public interface AppEvent {
-}
