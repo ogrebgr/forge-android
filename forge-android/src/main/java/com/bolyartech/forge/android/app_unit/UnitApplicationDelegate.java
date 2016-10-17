2ef93960a02acb18ef54ebe5cbd4dd1df7e66d1f
Ognyan Bankov <ogibankov@gmail.com>
2016-10-17 13:54:24 +0300

    adding file

---
+++ forge-android/src/main/java/com/bolyartech/forge/android/app_unit/UnitApplicationDelegate.java
@@ -0,0 +1,30 @@
+package com.bolyartech.forge.android.app_unit;
+
+import android.support.annotation.NonNull;
+
+
+/**
+ * Delegate to be used for unit base activities which implement UnitActivity interface
+ *
+ * @param <T> Type of the resident component
+ */
+public final class UnitApplicationDelegate<T> {
+    private T mResident;
+
+
+    public void setResident(@NonNull T resident) {
+        mResident = resident;
+    }
+
+
+    @NonNull
+    public T getResident() {
+        return mResident;
+    }
+
+
+    @NonNull
+    public T getRes() {
+        return getResident();
+    }
+}
