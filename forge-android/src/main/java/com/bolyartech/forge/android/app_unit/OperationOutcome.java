8f813d4c03a9c464e0bc06b61f837f57f02dca87
Ognyan Bankov <ogibankov@gmail.com>
2016-10-25 15:48:24 +0300

    removing isAborted

--- forge-android/src/main/java/com/bolyartech/forge/android/app_unit/OperationOutcome.java
+++ forge-android/src/main/java/com/bolyartech/forge/android/app_unit/OperationOutcome.java
@@ -2,32 +2,30 @@ package com.bolyartech.forge.android.app_unit;
 
 public final class OperationOutcome<RESULT, ERROR> {
     private final boolean mIsSuccessful;
-    private final boolean mIsAborted;
     private final RESULT mResult;
     private final ERROR mError;
 
 
     public static <RES, ERR> OperationOutcome<RES, ERR> createSuccessOutcome(RES result) {
-        return new OperationOutcome<>(true, false, result, null);
+        return new OperationOutcome<>(true, result, null);
     }
 
 
     public static <RES, ERR> OperationOutcome<RES, ERR> createErrorOutcome(ERR error) {
-        return new OperationOutcome<>(false, false, null, error);
+        return new OperationOutcome<>(false, null, error);
     }
 
 
     public static <RES, ERR> OperationOutcome<RES, ERR> createAbortedOutcome() {
-        return new OperationOutcome<>(false, true, null, null);
+        return new OperationOutcome<>(false, null, null);
     }
 
 
-    private OperationOutcome(boolean isSuccessful, boolean isAborted, RESULT result, ERROR error) {
+    private OperationOutcome(boolean isSuccessful, RESULT result, ERROR error) {
         // we don't check for valid combinations of the parameters because the only way to call this constructor is
         // via static factory methods
 
         mIsSuccessful = isSuccessful;
-        mIsAborted = isAborted;
         mResult = result;
         mError = error;
     }
@@ -35,11 +33,6 @@ public final class OperationOutcome<RESULT, ERROR> {
 
     public boolean isSuccessful() {
         return mIsSuccessful;
-    }
-
-
-    public boolean isAborted() {
-        return mIsAborted;
     }
 
 
