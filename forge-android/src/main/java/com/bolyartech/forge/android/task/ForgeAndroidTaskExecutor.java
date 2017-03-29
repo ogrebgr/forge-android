55a12ae9cd7e0b3d9612a2caff8a5f7a6600dc83
Ognyan Bankov <ogibankov@gmail.com>
2017-03-29 17:48:17 +0300

    adding constructors

--- forge-android/src/main/java/com/bolyartech/forge/android/task/ForgeAndroidTaskExecutor.java
+++ forge-android/src/main/java/com/bolyartech/forge/android/task/ForgeAndroidTaskExecutor.java
@@ -1,10 +1,26 @@
 package com.bolyartech.forge.android.task;
 
 import com.bolyartech.forge.base.exchange.forge.ForgeExchangeResult;
+import com.bolyartech.forge.base.misc.TimeProvider;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ScheduledExecutorService;
 
 
 /**
  * Task executor for Forge exchanges
  */
 public class ForgeAndroidTaskExecutor extends AndroidTaskExecutorImpl<ForgeExchangeResult> {
+    public ForgeAndroidTaskExecutor() {
+    }
+
+
+    public ForgeAndroidTaskExecutor(ExecutorService taskExecutorService, ScheduledExecutorService scheduler) {
+        super(taskExecutorService, scheduler);
+    }
+
+
+    public ForgeAndroidTaskExecutor(ExecutorService taskExecutorService, int ttlCheckInterval, int taskTtl, ScheduledExecutorService scheduler, TimeProvider timeProvider) {
+        super(taskExecutorService, ttlCheckInterval, taskTtl, scheduler, timeProvider);
+    }
 }
