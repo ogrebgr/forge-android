9bdcac62284df0f4d973feb0f6972859df38bc3c
Ognyan Bankov <ogibankov@gmail.com>
2018-07-06 15:53:12 +0300

    8.0.2 release

---
+++ forge-android/src/main/java/com/bolyartech/forge/android/app_unit/rc_task/RctResidentComponentAdapter.java
@@ -0,0 +1,20 @@
+package com.bolyartech.forge.android.app_unit.rc_task;
+
+
+import android.support.annotation.NonNull;
+
+import com.bolyartech.forge.android.app_unit.rc_task.executor.RcTaskExecutor;
+import com.bolyartech.forge.android.app_unit.rc_task.task.RcTaskToExecutor;
+
+
+public class RctResidentComponentAdapter extends AbstractRctResidentComponent {
+    public RctResidentComponentAdapter(RcTaskExecutor taskExecutor) {
+        super(taskExecutor);
+    }
+
+
+    @Override
+    protected void onTaskPostExecute(@NonNull RcTaskToExecutor endedTask) {
+        // empty on purpose
+    }
+}
