2345fe32d233204e6fbc8e631a83f6e71a27e4e5
Ognyan Bankov <ogibankov@gmail.com>
2018-07-29 20:30:21 +0300

    RcTaskResult constructor is now public

--- forge-android/src/main/java/com/bolyartech/forge/android/app_unit/rc_task/task/RcTaskResult.java
+++ forge-android/src/main/java/com/bolyartech/forge/android/app_unit/rc_task/task/RcTaskResult.java
@@ -1,9 +1,6 @@
 package com.bolyartech.forge.android.app_unit.rc_task.task;
 
 
-import static com.bolyartech.forge.base.misc.ForgeMessageFormat.format;
-
-
 public class RcTaskResult<SUCCESS_VALUE, ERROR_VALUE> {
     private final SUCCESS_VALUE successValue;
     private final ERROR_VALUE errorValue;
@@ -11,7 +8,7 @@ public class RcTaskResult<SUCCESS_VALUE, ERROR_VALUE> {
     private final boolean isSuccess;
 
 
-    RcTaskResult(boolean isSuccess, SUCCESS_VALUE successValue, ERROR_VALUE errorValue) {
+    public RcTaskResult(boolean isSuccess, SUCCESS_VALUE successValue, ERROR_VALUE errorValue) {
         this.isSuccess = isSuccess;
         this.successValue = successValue;
         this.errorValue = errorValue;
